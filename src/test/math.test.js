7329af123d95adce4a6440aabaec902b7584cf3f
Tomatsuppe11211 <142995184+Tomatsuppe11211@users.noreply.github.com>
2025-11-25 18:24:39 +0100

    added 3 extra multiplication scenarios

--- src/test/math.test.js
+++ src/test/math.test.js
@@ -1,5 +1,8 @@
 import { expect, test } from "vitest"
 import { multiply } from "../script/math.js"
+import { multiplyByZero } from "../script/math.js"
+import { multiplyByNegative } from "../script/math.js"
+import { multiplyByDecimals } from "../script/math.js"
 
 
 test("Multiplying numbers", () => {
@@ -8,3 +11,18 @@ test("Multiplying numbers", () => {
 
 
 //Note: This test works!
+
+//Adding the next three scenarios
+test("multiplying by zero", () => {
+    expect(multiplyByZero(4)).toBe(0)
+})
+
+test("multiplying by negative number", () => {
+    expect(multiplyByNegative(3, 4)).toBe(-12)
+})
+
+test("multiplying with decimals", () => {
+    expect(multiplyByDecimals(3, 4)).toBe(12.00)
+})
+
+//All scenarios works :) 
